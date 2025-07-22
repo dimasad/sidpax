666a3b431070cbaa2ef7ebd4379e0d62781f5b4f
Dimas Abreu Archanjo Dutra <dimas.dutra@mail.wvu.edu>
2025-07-22 13:23:39 -0400

    Implemented output residual cost Hessian.

--- src/sidpax/sem.py
+++ src/sidpax/sem.py
@@ -4,7 +4,6 @@ import typing
 
 import hedeut
 import jax
-import jax.flatten_util
 import jax.numpy as jnp
 import jax.scipy as jsp
 import jax_dataclasses as jdc
@@ -98,23 +97,38 @@ class Estimator:
         """Cost function gradient."""
         return jax.grad(self.cost, 1)(data, param)
 
-    def cost_2step_grad(self, data, param):
+    def yres_cost_hess(self, data, param, param_ind=None):
+        """Output residual cost-function sparse Hessian, in COO format."""
+        # Get the parameter indices, if needed
+        if param_ind == None:
+            param_ind = common.pytree_ind(param)
+
+        # Get the Cholesky factor of the residual covariance matrices
         Q_chol, R_chol = self.res_cov(data, param)
-        solve_triangular = jnp.vectorize(
-            lambda L, b: jsp.linalg.solve_triangular(L, b, lower=True),
-            signature="(n,n),(n)->(n)",
+
+        # Get the pytree of a single argument block
+        block_tree = jdc.replace(param, mu=param.mu[0])
+
+        # Ravel (flatten) a block and obtain the unravelling function
+        block, unravel_block = ravel_pytree(block_tree)
+
+        # Vectorized function for ravelling flattening all blocks into a matrix
+        ravel_yres_blocks = jax.vmap(
+            lambda tree: ravel_pytree(tree)[0],
+            in_axes=(self.Param(p=None, mu=0, Sigma_cond=None, S_cross=None),),
         )
 
-        def cost_step2(param):
-            xresn = solve_triangular(Q_chol, self.xres(data, param))
-            yresn = solve_triangular(R_chol, self.yres(data, param))
+        # Equivalent cost function for Hessian, using the squared error
+        def sqerr(param_block, datum):
+            param = unravel_block(param_block)
+            yres = self.yres_sample(datum, param)
+            yresn = jsp.linalg.solve_triangular(R_chol, yres.T, lower=True).T
+            return jnp.sum(yresn**2, axis=1).mean(axis=0)
 
-            xsq_err = 0.5 * jnp.sum(xresn**2, (0, 2)).mean()
-            ysq_err = 0.5 * jnp.sum(yresn**2, (0, 2)).mean()
-            entropy = self.state_path_entropy(param.Sigma_cond, len(data))
-            return xsq_err + ysq_err - entropy
-
-        return jax.grad(cost_step2)(param)
+        val = jax.vmap(jax.hessian(sqerr))(ravel_yres_blocks(param), data)
+        row = jnp.repeat(ravel_yres_blocks(param_ind), len(block), -1)
+        col = jnp.tile(ravel_yres_blocks(param_ind), (1, len(block)))
+        return val, (row, col)
 
     def entropy_hess_coo(self, data, param: Param, param_ind=None):
         """Entropy Hessian in COO format, but it will always be zero..."""
@@ -156,17 +170,20 @@ class Estimator:
         return jax.vmap(self.yres_sample, in_axes)(data, param)
 
     def xres_args(self, data, param: Param):
-        return dict(
-            mu_next=param.mu[1:],
-            mu_curr=param.mu[:-1],
-            u_curr=data.u[:-1],
+        return (
+            data.u[:-1],
+            param.mu[1:],
+            param.mu[:-1],
+            param.p,
+            param.Sigma_cond,
+            param.S_cross,
         )
 
-    def xres_sample(self, mu_next, mu_curr, u_curr, param: Param):
+    def xres_sample(self, u_curr, mu_next, mu_curr, p, Sigma_cond, S_cross):
         """Discrete-time state transition residuals for a single sample pair."""
         # Get the Cholesky factor of the marginal state covariance
-        S_cond = param.Sigma_cond.chol
-        S_marg = common.tria_qr(S_cond, param.S_cross)
+        S_cond = Sigma_cond.chol
+        S_marg = common.tria_qr(S_cond, S_cross)
 
         # Get the standard normal sigma points and weights.
         # Note that weights will be discarded, as they are all equal.
@@ -175,7 +192,7 @@ class Estimator:
 
         # Scale the sigma points
         x_curr_dev = jnp.matvec(S_marg, std_dev[:, :nx])
-        x_next_dev = jnp.matvec(param.S_cross, std_dev[:, :nx]) + jnp.matvec(
+        x_next_dev = jnp.matvec(S_cross, std_dev[:, :nx]) + jnp.matvec(
             S_cond, std_dev[:, -nx:]
         )
 
@@ -184,10 +201,10 @@ class Estimator:
         x_next = mu_next + x_next_dev
 
         # Return the residuals
-        return x_next - x_curr - self.model.f(x_curr, u_curr, param.p)
+        return x_next - x_curr - self.model.f(x_curr, u_curr, p)
 
     def xres(self, data: Data, param: Param):
         """Discrete-time state transition residuals for a full trajectory."""
-        xres_sample_vmap = jax.vmap(self.xres_sample, in_axes=(0, 0, 0, None))
-        xres_args = self.xres_args(data, param).values()
-        return xres_sample_vmap(*xres_args, param)
+        in_axes = 0, 0, 0, None, None, None
+        xres_sample_vmap = jax.vmap(self.xres_sample, in_axes=in_axes)
+        return xres_sample_vmap(*self.xres_args(data, param))
