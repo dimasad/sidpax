e72299cf52163866d906aeeadadf32a5a7c0ccf3
Dimas Abreu Archanjo Dutra <dimas.dutra@mail.wvu.edu>
2025-07-25 16:50:39 -0400

    Added base class for binding parameters to models.

---
+++ src/sidpax/modeling.py
@@ -0,0 +1,46 @@
+"""Classes for representing dynamic system models."""
+
+from dataclasses import dataclass
+import functools
+import inspect
+from typing import Any
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+
+class StateSpaceBase:
+    """Base class for state-space model."""
+
+    def bind(self, **kwargs):
+        return PartialObject(self, **kwargs)
+
+
+class PartialObject:
+    """Object whose methods are bound to specific values."""
+
+    def __init__(self, obj, **kwargs):
+        self._obj = obj
+        """Underlying object."""
+
+        self._kwargs = kwargs
+        """Dictionary of values bound to argument names."""
+
+    def __getattr__(self, name):
+        # Get underlying attribute
+        attr = getattr(self._obj, name)
+
+        # If `name` is a property, return it
+        if not callable(method):
+            return attr
+        
+        # Get method signature
+        method = attr
+        sig = inspect.signature(method)
+
+        # Bind kwargs
+        for key, val in self._kwargs:
+            if key in sig.parameters:
+                method = functools.partial(method, **{key:val})
+        return method
