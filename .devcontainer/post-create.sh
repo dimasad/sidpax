644bfc666afdad9acddf94b2187d762fe1391a2a
Dimas Abreu Archanjo Dutra <dimas.dutra@mail.wvu.edu>
2025-11-03 21:08:28 +0000

    Added Cuda support to Dev Container.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,5 +7,8 @@ python -m venv .venv
 echo "Activating the virtual environment..."
 source .venv/bin/activate
 
+echo "Installing jax with GPU support..."
+pip install -U 'jax[cuda12]'
+
 echo "Installing the package with dependencies..."
 pip install -e ".[devextra,test]"
